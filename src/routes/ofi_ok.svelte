1ecf321195eeb5d5bddcdef189060c13dcff7550
ideadiego <dvalenzuela3@uc.cl>
2022-04-19 21:43:18 -0400

    ard mierc

---
+++ src/routes/ofi_ok.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import firebaseStore from "../stores/firebaseStore";
+    import { doc, getDoc, setDoc, serverTimestamp, arrayUnion, Timestamp, query, where, collection, getDocs, addDoc } from "firebase/firestore";
+    import authStore from "../stores/authStore";
+    
+    import { addToast } from 'as-toast';
+
+    
+    
+    </script>
+    
+    
+    <svelte:head>
+        <title>Gente en la ofi</title>
+    </svelte:head>
+
+    
